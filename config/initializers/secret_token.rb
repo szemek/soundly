032946ecb6ea9697bfc38be0992a187e8af2a563
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-06-15 15:35:10 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resound::Application.config.secret_token = '1bca3f59de591d6f914bed5cd0802cb10239c86e31e8865c035c16ce5fbad65d282c93d81a8669a6e34fad94aa7337f374a08f7dea5e0fcbb48b90daf9f25c10'
