7d2601c8dde8e863a415a8c6b7c950bfc734f573
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-07-06 20:20:52 +0200

    go back to full update

--- app/domain/tracks/updater.rb
+++ app/domain/tracks/updater.rb
@@ -1,32 +1,14 @@
 class Tracks::Updater
   @@tracks_per_page = 200
 
-  def self.run(update_type = nil)
-    pages = case update_type
-    when :complete_update then complete_update
-    when :diff_update then diff_update
-    else diff_update
-    end
+  def self.run
+    pages, rest = Tracks::Info.total_tracks.divmod(@@tracks_per_page)
+    pages += 1 if rest > 0
 
     (1..pages).each do |page|
       Rails.logger.info "#{page}/#{pages}"
       update_page(page)
     end
-  end
-
-  def self.complete_update
-    pages, rest = Tracks::Info.total_tracks.divmod(@@tracks_per_page)
-    pages += 1 if rest > 0
-
-    pages
-  end
-
-  def self.diff_update
-    diff = Tracks::Info.total_tracks - Track.count
-    pages, rest = diff.divmod(@@tracks_per_page)
-    pages += 1 if rest > 0
-
-    pages
   end
 
   def self.update_page(page)
