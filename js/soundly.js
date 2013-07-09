a1f25e198b0c05f19dec1aa8e49afd3b6632299c
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-07-09 09:41:04 +0200

    checking current artist and updating tracks on heroku

--- js/soundly.js
+++ js/soundly.js
@@ -1,4 +1,6 @@
 require(['$api/models'], function(models) {
+  var player = models.player;
+  player.load('track');
 
   $('#search').on('keyup', function(event) {
     var name = $(this).val();
@@ -12,5 +14,31 @@ require(['$api/models'], function(models) {
       });
     });
   });
+
+  $('#check-artist').on('click', function(event) {
+    try {
+      player.load('track');
+      var artist = _.first(player.track.artists);
+
+      $.get('http://soundly.herokuapp.com/tracks/search', {name: artist.name}, function(tracks){
+        $('#tracks tr').remove();
+
+        _.each(tracks, function(track){
+          var compiled = _.template($('#track-item').html());
+          var item = compiled({track: track});
+          $('#tracks').append(item)
+        });
+      });
+    }
+    catch(e) {
+      console.log(e);
+      console.log('Could not fetch artist');
+    }
+  });
+
+  $('#update-tracks').on('click', function(event) {
+    console.log('Updating...');
+    $.post('http://soundly.herokuapp.com/tracks/update', {pages: 1});
+  })
 
 });
