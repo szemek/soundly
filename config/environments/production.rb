00e56899fe59d9d198a7be1e99cd8da589201d44
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-07-27 10:58:44 +0200

    faster heroku deploys
    http://blog.alexmaccaw.com/faster-deploys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Soundly::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.cache_store = :dalli_store
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
