82dafaf6bf3b3a7cc9f1bdb7385b5552f496c066
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-08-03 20:39:15 +0200

    Refactored & simplified code for searching tracks

--- js/soundly.js
+++ js/soundly.js
@@ -1,44 +1,35 @@
 require(['$api/models'], function(models) {
-  var player = models.player;
-  player.load('track');
-
-  $('#search').on('keyup', function(event) {
-    var name = $(this).val();
-    $.get('http://soundly.herokuapp.com/tracks/search', {name: name}, function(tracks){
+  var search = function(query) {
+    $.get('http://soundly.herokuapp.com/tracks/search', {name: query}, function(tracks){
       $('#tracks tr').remove();
 
       _.each(tracks, function(track){
         var compiled = _.template($('#track-item').html());
         var item = compiled({track: track});
-        $('#tracks').append(item)
+        $('#tracks').append(item);
       });
     });
+  };
+
+  search();
+
+  var player = models.player;
+  player.load('track');
+
+  $('#search').on('keyup', function(event) {
+    var name = $(this).val();
+    search(name);
   });
 
-  $('#check-artist').on('click', function(event) {
+  $('#check').on('click', function(event) {
     try {
       player.load('track');
       var artist = _.first(player.track.artists);
-
-      $.get('http://soundly.herokuapp.com/tracks/search', {name: artist.name}, function(tracks){
-        $('#tracks tr').remove();
-
-        _.each(tracks, function(track){
-          var compiled = _.template($('#track-item').html());
-          var item = compiled({track: track});
-          $('#tracks').append(item)
-        });
-      });
+      search(artist.name);
     }
     catch(e) {
       console.log(e);
       console.log('Could not fetch artist');
     }
   });
-
-  $('#update-tracks').on('click', function(event) {
-    console.log('Updating...');
-    $.post('http://soundly.herokuapp.com/tracks/update', {pages: 1});
-  })
-
 });
