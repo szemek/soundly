609c0ad11faaf3c4167902e32ab4bd61af3187c5
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-10-23 09:26:10 +0200

    activity punchcard

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -2,5 +2,7 @@ class ActivityController < ApplicationController
   def show
     activity = Tracks::Activity.new
     @data = activity.gather
+    punchcard = Builder.new
+    @svg = punchcard.generate(@data)
   end
 end
--- app/domain/tracks/activity.rb
+++ app/domain/tracks/activity.rb
@@ -8,7 +8,7 @@ class Tracks::Activity
   def gather
     datetimes = Track.pluck(:uts).map{|e| Time.at(e)}
 
-    by_day = datetimes.group_by{|e| e.strftime('%A')}
+    by_day = datetimes.group_by{|e| e.strftime('%u').to_i - 1}
     by_day_and_hour = {}
 
     by_day.each do |day, times|
