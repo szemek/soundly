6d47ade42793cecd6ae228c3c89f2dfcdfdbff40
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-04-13 07:33:09 +0200

    more specific listener for player's track change

--- js/flow.js
+++ js/flow.js
@@ -1,11 +1,12 @@
 require(['$api/models'], function(models) {
   var player = models.player;
-  player.addEventListener('change', function(event) {
+  player.addEventListener('change:track', function(event) {
     player.load('track');
 
     var track = player.track;
     if(track.advertisement) {
       player.seek(track.duration - 1);
+      player.play();
     }
   });
 });
