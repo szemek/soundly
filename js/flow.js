de592a08dcf4260fbc77b95b6fe2452a2b46dc8e
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-06-01 17:59:59 +0200

    rewritten code to use .done method

--- js/flow.js
+++ js/flow.js
@@ -1,13 +1,14 @@
 require(['$api/models'], function(models) {
   var player = models.player;
   player.addEventListener('change:track', function(event) {
-    player.load('track');
+    player.load('track').done(function(){
+      var track = player.track;
 
-    var track = player.track;
-    if(track.advertisement) {
-      player.seek(track.duration - 1);
-      player.play();
-    }
+      if(track.advertisement) {
+        player.seek(track.duration - 1);
+        player.play();
+      }
+    });
   });
 });
 
