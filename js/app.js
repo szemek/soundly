cfedd0b3517d8fde54fe02c7deb3e1177569b79d
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-06-03 17:06:37 +0200

    updated all activity url

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ app.controller('ScrobblesController', ['$scope', '$http', function($scope, $http
 }]);
 
 app.controller('ActivityController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
-  var activity_svg_url = 'http://soundly.herokuapp.com/activity.svg';
+  var activity_svg_url = 'http://soundly.herokuapp.com/activity/all.svg';
 
   $http.get(activity_svg_url).success(function(data){
     $scope.svg = $sce.trustAsHtml(data);
