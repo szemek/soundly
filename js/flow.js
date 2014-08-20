3f13e952464f9f4e2d2fc64dbb3aa1734cc4223d
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-08-20 22:37:31 +0200

    skip ad when loading soundly app

--- js/flow.js
+++ js/flow.js
@@ -1,14 +1,21 @@
 require(['$api/models'], function(models) {
   var player = models.player;
-  player.addEventListener('change:track', function(event) {
+
+  var skip = function (){
     player.load('track').done(function(){
       var track = player.track;
 
-      if(track.advertisement) {
+      if(track && track.advertisement) {
         player.seek(track.duration - 1);
         player.play();
       }
     });
+  };
+
+  player.addEventListener('change:track', function() {
+    skip();
   });
+
+  skip();
 });
 
